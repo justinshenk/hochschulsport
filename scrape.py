5417a9f003b8c3eb08eea431c24f16e4eb34a1f9
themightyoarfish <rdiederichse@uos.de>
2017-03-01 17:42:24 +0100

    Commenting

--- scrape.py
+++ scrape.py
@@ -1,27 +1,42 @@
+"""
+Module for scraping the Hochschulsport website
+"""
 import requests
 from urllib import parse
-from bs4 import BeautifulSoup
 import pprint
+from filters import filter_early_ss17
 
-def get_courses(course_list_url, course_filter):
-    html = requests.get(course_list_url).text
-    links, names = course_filter(html)
-    for i, (l, n) in enumerate(zip(links, names)):
-        if 'RESTPLÄTZE' in n:
-            del links[i]
-            del names[i]
-    links = map(lambda l: parse.urljoin(course_list_url, l), links)
-    return list(zip(links, names))
+class Scraper(object):
+
+    """Class to scrape yo ass."""
+
+    def __init__(self, course_list_url, course_filter):
+        """Create a new scraper
+
+        :course_list_url: The URL wherein all current courses are listed
+        :course_filter: Function of one parameter (the html text) which should
+            extract (link, name) tuples from the html
+        """
+        self._course_list_url = course_list_url
+        self._course_filter = course_filter
+        
+
+    def get_courses(self):
+        """Extract courses from the index page.
+
+        :return: list of tuple of (abs_link, name)
+        """
+        html = requests.get(self._course_list_url).text
+        links, names = self._course_filter(html)
+        for i, (l, n) in enumerate(zip(links, names)):
+            if 'RESTPLÄTZE' in n:
+                del links[i]
+                del names[i]
+        links = map(lambda l: parse.urljoin(course_list_url, l), links)
+        return list(zip(links, names))
 
 if __name__ == "__main__":
 
     course_list_url = 'https://buchung.zfh.uni-osnabrueck.de/angebote/aktueller_zeitraum/index.html'
-
-    def filter(html):
-        soup = BeautifulSoup(html)
-        a_elems = soup.select('dl.bs_menu > dd a')
-        links = map(lambda l: l.attrs['href'], a_elems) 
-        names = map(lambda l: l.text, a_elems)
-        return list(links), list(names)
-
-    pprint.pprint(get_courses(course_list_url, filter))
+    s = Scraper(course_list_url, filter_early_ss17)
+    pprint.pprint(s.get_courses())
