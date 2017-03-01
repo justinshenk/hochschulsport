40cca92f2e4bbd263a5a2b472a34dacd6be47c75
themightyoarfish <rdiederichse@uos.de>
2017-03-01 19:36:57 +0100

    Fetch specialisations/offerings of each course

---
+++ course.py
@@ -0,0 +1,31 @@
+class Course(object):
+
+    def __init__(self, id, name, url=None, time=None, place=None):
+        self.__id = id
+        self.__name = name
+        self.__url = url
+        self.__time = time
+        self.__place = place
+
+    def __str__(self):
+        return "{name}: {time}".format(name=self.name, time=self.time)
+
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def url(self):
+        return self.__url
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def time(self):
+        return self.__time
+    
+    @property
+    def place(self):
+        return self.__place
--- scrape.py
+++ scrape.py
@@ -1,10 +1,13 @@
 """
 Module for scraping the Hochschulsport website
 """
-import requests
+from bs4 import BeautifulSoup
 from urllib import parse
 import pprint
+import requests
+
 from filters import filter_early_ss17
+from course import Course
 
 class Scraper(object):
 
@@ -20,23 +23,45 @@ class Scraper(object):
         self._course_list_url = course_list_url
         self._course_filter = course_filter
         
+    def get_id(self, course_url):
+        html = requests.get(course_url).text
 
     def get_courses(self):
         """Extract courses from the index page.
 
-        :return: list of tuple of (abs_link, name)
+        :return: list of Course objects
         """
         html = requests.get(self._course_list_url).text
         links, names = self._course_filter(html)
-        for i, (l, n) in enumerate(zip(links, names)):
-            if 'RESTPLÄTZE' in n:
-                del links[i]
-                del names[i]
         links = map(lambda l: parse.urljoin(course_list_url, l), links)
-        return list(zip(links, names))
+        all_courses = []
+
+        def course_filter_detail(html):
+            soup = BeautifulSoup(html)
+            name = soup.find('div', class_='bs_head').text
+            rows = soup.select('table.bs_kurse tbody tr')
+            courses = []
+            for row in rows:
+                try:
+                    id = row.find('input').attrs['name']
+                except AttributeError:
+                    continue
+                else:
+                    course_name = name + ' ' + row.find('td', class_='bs_sdet').text
+                    time = row.find('td', class_='bs_szeit').text
+                    courses.append(Course(id, course_name, time=time))
+            return courses
+
+        for l in links:
+            html = requests.get(l).text
+            all_courses.extend(course_filter_detail(html))
+
+        return all_courses
 
 if __name__ == "__main__":
 
     course_list_url = 'https://buchung.zfh.uni-osnabrueck.de/angebote/aktueller_zeitraum/index.html'
     s = Scraper(course_list_url, filter_early_ss17)
-    pprint.pprint(s.get_courses())
+    courses = s.get_courses()
+    for c in courses:
+        print(c)
