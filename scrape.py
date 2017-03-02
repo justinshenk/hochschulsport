4dd7e8ffaf4ac3ae4293817c3e9ab826f8ebd978
themightyoarfish <rdiederichse@uos.de>
2017-03-02 17:44:33 +0100

    use async requests with 5 retries

--- scrape.py
+++ scrape.py
@@ -3,65 +3,92 @@ Module for scraping the Hochschulsport website
 """
 from bs4 import BeautifulSoup
 from urllib import parse
-import pprint
+from pickle import dump, load
+from sys import stderr
+from os.path import exists
+from requests_futures.sessions import FuturesSession
 import requests
+from requests.adapters import HTTPAdapter
+import pprint
 
-from filters import filter_early_ss17
+from filters import course_links_early_ss17, course_filter_detail_early_ss17
 from course import Course
 
 class Scraper(object):
 
     """Class to scrape yo ass."""
 
-    def __init__(self, course_list_url, course_filter):
+    def __init__(self, course_list_url, course_link_filter, course_filter, fname='courses.pickle'):
         """Create a new scraper
 
         :course_list_url: The URL wherein all current courses are listed
-        :course_filter: Function of one parameter (the html text) which should
+        :course_link_filter: Function of one parameter (the html text) which should
             extract (link, name) tuples from the html
+        :course_filter: Function of one parameter (the html text) to extract
+            from each course page the individual courses (there may be several time
+            slots or different offerings)
+        :fname: File name to save the courses in
         """
-        self._course_list_url = course_list_url
-        self._course_filter = course_filter
-        
-    def get_id(self, course_url):
-        html = requests.get(course_url).text
+        self.__course_list_url = course_list_url
+        self.__course_link_filter = course_link_filter
+        self.__course_filter = course_filter
+        self.__fname = fname
+        self.__courses = None
 
-    def get_courses(self):
+    @property
+    def courses(self):
+        if not self.__courses:
+            stderr.write(('Courses not loaded,'
+                'attempting to read from {}...\n').format(self.__fname))
+            if not exists(self.__fname):
+                stderr.write('File not found, downloading...\n')
+                self.update_courses()
+            else:
+                self.load_courses()
+        return self.__courses
+        
+    def save_courses(self):
+        with open(self.__fname, mode='wb') as file:
+            dump(self.__courses, file)
+
+    def load_courses(self):
+        with open(self.__fname, mode='rb') as file:
+            self.__courses = load(file)
+
+    def update_courses(self):
         """Extract courses from the index page.
 
         :return: list of Course objects
         """
-        html = requests.get(self._course_list_url).text
-        links, names = self._course_filter(html)
-        links = map(lambda l: parse.urljoin(course_list_url, l), links)
-        all_courses = []
+        session = requests.Session()
+        retry_adapter = HTTPAdapter(max_retries=5)
+        session.mount(self.__course_list_url, retry_adapter)
+        html = session.get(self.__course_list_url).text
+        links, names = self.__course_link_filter(html)
+        links = list(map(lambda l: parse.urljoin(course_list_url, l), links))
 
-        def course_filter_detail(html):
-            soup = BeautifulSoup(html)
-            name = soup.find('div', class_='bs_head').text
-            rows = soup.select('table.bs_kurse tbody tr')
-            courses = []
-            for row in rows:
-                try:
-                    id = row.find('input').attrs['name']
-                except AttributeError:
-                    continue
-                else:
-                    course_name = name + ' ' + row.find('td', class_='bs_sdet').text
-                    time = row.find('td', class_='bs_szeit').text
-                    courses.append(Course(id, course_name, time=time))
-            return courses
+        session = FuturesSession()
+        # we assume the individual courses have the same prefix as the course
+        # list
+        parsed_url = parse.urlparse(self.__course_list_url)
+        prefix = parsed_url.scheme + '://' + parsed_url.netloc
+        session.mount(prefix, retry_adapter)
+        futures = []
+        for i, l in enumerate(links):
+            print('Processing {} ({} of {})'.format(l, i+1, len(links)))
+            futures.append(session.get(l, timeout=20))
+            # all_courses.extend(self.__course_filter(html))
 
-        for l in links:
-            html = requests.get(l).text
-            all_courses.extend(course_filter_detail(html))
-
-        return all_courses
+        self.__courses = [course for lst in
+                (self.__course_filter(f.result().text) for f in futures if not
+                    f.exception()) for course in lst]
 
 if __name__ == "__main__":
 
     course_list_url = 'https://buchung.zfh.uni-osnabrueck.de/angebote/aktueller_zeitraum/index.html'
-    s = Scraper(course_list_url, filter_early_ss17)
-    courses = s.get_courses()
+    s = Scraper(course_list_url, course_links_early_ss17,
+            course_filter_detail_early_ss17)
+    s.update_courses()
+    courses = s.courses
     for c in courses:
         print(c)
