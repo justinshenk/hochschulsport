30405594cb60dba57b25bed85a510fb347de5508
themightyoarfish <rdiederichse@uos.de>
2017-03-13 13:01:17 +0100

    add filter for fid extraction

--- filters.py
+++ filters.py
@@ -15,10 +15,10 @@ def course_links_early_ss17(html):
     names = map(lambda l: l.text, a_elems)
     return list(links), list(names)
 
-"""
-Extract specialisations of a course from its page
-"""
 def course_filter_detail_early_ss17(html):
+    """
+    Extract specialisations of a course from its page
+    """
     soup = BeautifulSoup(html)
     name = soup.find('div', class_='bs_head').text
     rows = soup.select('table.bs_kurse tbody tr')
@@ -30,8 +30,16 @@ def course_filter_detail_early_ss17(html):
             print('Skipping {}...'.format(name), file=sys.stderr)
             continue
         else:
+            kind = course_row.attrs['value']
             id = int(re.match(r'BS_Kursid_(\d+)', course_row.attrs['name']).group(1))
             course_name = name + ' ' + row.find('td', class_='bs_sdet').text
             time = row.find('td', class_='bs_stag').text + ' ' + row.find('td', class_='bs_szeit').text
-            courses.append(Course(id, course_name, time=time))
+            courses.append(Course(id, course_name, time=time, kind=kind))
     return courses
+
+def extract_fid(html):
+    fid_input = BeautifulSoup(html).select('input[name="fid"]')
+    if not fid_input:
+        return None
+    else:
+        return fid_input[0].attrs['value']
